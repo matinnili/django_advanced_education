1575cceb99f78a0ef210063decfc87479ae04051
Matin nili <matinnili94@gmail.com>
2024-04-05 18:03:00 +0330

    pagianation and ordering was added to listview

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import indexview,IndexView
+from .views import indexview,IndexView,Redirecttomaktab,PostList
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
@@ -7,5 +7,6 @@ app_name='blog'
 urlpatterns = [
     # path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context={"name":"ali"})),
     path('cbv-index/',IndexView.as_view(),name='cbv-index'),
-    path('go-to-index',RedirectView.as_view(pattern_name='blog:cbv-index'))
+    path('post/',PostList.as_view(),name='post-list'),
+    path('go-to-index/<int:pk>',Redirecttomaktab.as_view())
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,12 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from .models import Post
+
 # Create your views here.
 def indexview(request):
     """
@@ -18,3 +24,20 @@ class IndexView(TemplateView):
         context=super().get_context_data(**kwargs)
         context['name']='ali'
         return context
+class Redirecttomaktab(RedirectView):
+    url='https://maktabkhooneh.com'
+    def get_redirect_url(self, *args: Any, **kwargs: Any) :
+        post=get_object_or_404(Post,pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+    
+    
+class PostList(ListView):
+    # model=Post
+    # queryset=Post.objects.all()
+    context_object_name='Posts'
+    paginate_by=2
+    ordering='id'
+    def get_queryset(self) :
+        Posts=Post.objects.filter(status=False)
+        return Posts
