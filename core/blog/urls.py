3f63a43520fa6cffe09962087900baebe08a15e0
Matin nili <matinnili94@gmail.com>
2024-04-05 10:48:44 +0330

    redirect view was added

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import indexview,IndexView
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+app_name='blog'
 urlpatterns = [
-    path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context={"name":"ali"})),
-    path('fbv-index/',IndexView.as_view()
-         ),
+    # path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context={"name":"ali"})),
+    path('cbv-index/',IndexView.as_view(),name='cbv-index'),
+    path('go-to-index',RedirectView.as_view(pattern_name='blog:cbv-index'))
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 # Create your views here.
 def indexview(request):
+    """
+    a function based view  to show index page    
+    """
     context={'name':'ali'}
     return render(request,"index.html",context)
+
 class IndexView(TemplateView):
+    """
+    a class based view  to show index page
+    """
     template_name='index.html'
     def get_context_data(self, **kwargs: Any) :
         context=super().get_context_data(**kwargs)
